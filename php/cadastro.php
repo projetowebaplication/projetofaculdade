b58520bcfb928c15b8429eb8ee0a58099fa08951
Karinaneri <karinanerisantos@gmail.com>
2019-06-01 14:07:54 -0300

    Alteração no commit

---
+++ php/cadastro.php
@@ -0,0 +1,14 @@
+<?php
+
+$name=$_POST ["name"];
+$email=$_POST ["email"];
+$username=$_POST ["username"];
+$password=$_POST ["password"];
+$confirm=$_POST ["confirm"];
+echo "Nome:$name<br>";
+echo "email:$email<br>";
+echo "username:$username<br>";
+echo "password:$password<br>";
+echo "Confirmação: $confirm<br>";
+
+
